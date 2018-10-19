8c5ba5e40abc3472e571d211ff4974debf047987
Alex Gilleran <alex@alexgilleran.com>
2018-10-19 22:17:09 +1100

    Fixed up some bad if-ing

--- src/components/test-if.jsx
+++ src/components/test-if.jsx
@@ -4,7 +4,7 @@ export default function TestIf() {
   return (
     <div>
       <h1>If Test</h1>
-      <If condition>This should show</If>
+      <If condition={true}>This should show</If>
       <If condition={false}>This should not show</If>
     </div>
   );
